6529decebab62beb9ecfd9ef51cff50e536f818a
cereceres <jesus.edelcereceres@gmail.com>
2018-09-14 13:52:11 -0600

    the refactorization is done, only the value is obtained from dict and not the printer

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const printDoct = {
-    '0IsMultipleOfThree': () => console.log('Linio'),
-    '0IsMultipleOfFive': () => console.log('IT'),
-    '0IsMultipleOfThreeAndFive': () => console.log('Linianos')
+const valueDoct = {
+    '0IsMultipleOfThree': 'Linio',
+    '0IsMultipleOfFive': 'IT',
+    '0IsMultipleOfThreeAndFive': 'Linianos'
 };
 
-const defaultPrint = (i) => console.log(i);
-const getPrinter = (i) => printDoct[`${i % 3 + i % 5 }IsMultipleOfThreeAndFive`] ||
-printDoct[`${i % 3 }IsMultipleOfThree`] ||
-printDoct[`${i % 5 }IsMultipleOfFive`];
+const defaultPrint = (i) => i;
+const getPrinted = (i) => valueDoct[`${i % 3 + i % 5 }IsMultipleOfThreeAndFive`] ||
+valueDoct[`${i % 3 }IsMultipleOfThree`] ||
+valueDoct[`${i % 5 }IsMultipleOfFive`];
 
 
 const printNumbersTo = (until = 100) => {
     for (let i = 1, len = until + 1; i < len; i++) {
-        let printer = getPrinter(i);
-        if (!printer) printer = defaultPrint;
-        printer(i);
+        let value = getPrinted(i);
+        if (!value) value = defaultPrint(i);
+        console.log(value);
     }
 };
 
